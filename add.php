2bf746ea6e6a26f43dfe6da290851c20646cf271
selim song <491497047@qq.com>
2013-08-25 17:03:51 +0800

    m

--- add.php
+++ add.php
@@ -7,14 +7,14 @@
 <?php
 if(!empty($_POST['title'])){
 $m = new mongoClient('mongodb://127.0.0.1', array());
-$db = $m->star;
-$collection = $db->mei;
+$db = $m->mei;
+$collection = $db->goods;
 $doc = array('title' => trim($_POST['title']), 'price' => trim($_POST['price']), 'website' => trim($_POST['website']), 'imageurl' => addslashes(trim($_POST['imageurl'])), 'gstatus' => 1,'updatetime'=> time());
 $collection->insert($doc);
 
 }
  ?>
-<form>
+<form action="" method="post">
 title   :<input  type="text" name="title" value=""   /><br />
 price   :<input  type="text" name="price" value=""   /><br />
 website :<input  type="text" name="website"   value="京东"  /><br />
---
+++ list.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<table width="1215" height="240" border="1">
+<?php
+$m = new mongoClient('mongodb://127.0.0.1', array());
+$db = $m->mei;
+$collection = $db->goods;
+$cursor = $collection->find()->limit(10);
+foreach ($cursor as $doc) {
+ ?>
+  <tr bordercolor="#FF0000">
+    <td width="319">title: <?php echo $doc['title'] ?></td>
+    <td width="98">price: <?php echo $doc['price'] ?></td>
+    <td width="107">website: <?php echo $doc['website'] ?></td>
+    <td width="392">image:  <img border="0" src="<?php echo $doc['imageurl'] ?>"  ></td>
+    <td width="121">goodsstatus: <?php echo $doc['gstatus'] ?></td>
+    <td width="138">updatetime: <?php echo $doc['updatetime'] ?></td>
+    <td width="edit"><a href="edit.php?id=<?php echo $doc['_id']->{'$id'} ?>">编辑</a></td>
+  </tr>
+<?php
+}
+?>
+
+</table>
+
+
+
+</body>
+</html>
